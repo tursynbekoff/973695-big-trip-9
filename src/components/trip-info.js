590fc62089ec048e3e2c81c118a9437e59d1fe3a
tursynbekoff <37927630+tursynbekoff@users.noreply.github.com>
2019-08-21 23:52:19 +0600

    Generating moch data and inserting data into template

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -1,7 +1,23 @@
-export const tripInfo = () => {
-  return `<div class="trip-info__main">
-    <h1 class="trip-info__title">Amsterdam &mdash; ... &mdash; Amsterdam</h1>
+export const tripInfo = (initialDesination, lastDestination, beginningDate, lastDate) => {
+  const months = [`January`,
+    `February`,
+    `March`,
+    `April`,
+    `May`,
+    `June`,
+    `July`,
+    `August`,
+    `September`,
+    `October`,
+    `November`,
+    `December`];
 
-    <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
-  </div>`;
+  const makeTripDestination = (originalDesination, finallDestination, startDate, endDate) => `<div class="trip-info__main">
+    <h1 class="trip-info__title">${originalDesination} &mdash; ... &mdash; ${finallDestination}</h1>
+
+    <p class="trip-info__dates">${months[new Date(startDate).getMonth()]} ${new Date(startDate).getDate()}&nbsp;&mdash;&nbsp;
+    ${months[new Date(endDate).getMonth()]} ${new Date(endDate).getDate()}</p>
+  </div>`.trim();
+
+  return makeTripDestination(initialDesination, lastDestination, beginningDate, lastDate);
 };
