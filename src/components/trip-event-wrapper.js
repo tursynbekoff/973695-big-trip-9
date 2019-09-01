108b85099f53509be844fa659e65e9f2ded24b59
tursynbekoff <37927630+tursynbekoff@users.noreply.github.com>
2019-09-01 23:38:56 +0600

    adding classes and making switchable event diplay/edit

--- src/components/trip-event-wrapper.js
+++ src/components/trip-event-wrapper.js
@@ -1,5 +1,9 @@
+import {data} from './data.js';
 import {tripEvents} from './trip-events.js';
+import {tripEventEdit} from './trip-event-edit.js';
 export const tripEventWrapper = (object) => {
+  const TripDestination = tripEvents();
+  const TripEditDestination = tripEventEdit();
 
   const months = [`Jan`,
     `Feb`,
@@ -13,15 +17,65 @@ export const tripEventWrapper = (object) => {
     `Oct`,
     `Nov`,
     `Dec`];
+  const renderEvent = (eventMock) => {
+    const eventDisplay = new TripDestination(eventMock);
+    const eventEdit = new TripEditDestination(eventMock);
 
-  const makeTripDestination = (data) =>`<ul class="trip-days">
-      <li class="trip-days__item  day">
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        eventContainer.replaceChild(eventDisplay.getElement(), eventEdit.getElement());
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+    const eventInDom = document.querySelector(`.event__rollup-btn`);
+    console.log(eventInDom);
+
+    eventDisplay.getElement()
+    .querySelector(`.event__rollup-btn`)
+    .addEventListener(`click`, () => {
+      console.log(`kekistan!`);
+      eventContainer.replaceChild(eventEdit.getElement(), eventEdit.getElement());
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+    return eventDisplay.getTemplate();
+  };
+
+  const eventContainer = document.querySelector(`.trip-events__list`);
+  console.log(eventContainer);
+  const EVENT_COUNT = 3;
+
+  const eventMocks = new Array(EVENT_COUNT)
+                    .fill(``)
+                    .map(data);
+
+  const sortedEventMocks = eventMocks.sort(function (a, b) {
+    const nextValue = (a.intermediateStartTime.replace(`:`, ``));
+    const currentValue = (b.intermediateStartTime.replace(`:`, ``));
+    return nextValue - currentValue;
+  });
+
+  let totalPrice = 0;
+
+  sortedEventMocks.forEach((eachEvent) => {
+    totalPrice += eachEvent.intermediatePrice + eachEvent.optionCost[0];
+  });
+
+  const tripCost = document.querySelector(`.trip-info__cost-value`);
+  tripCost.innerText = totalPrice;
+
+  const concatinatedEvents = sortedEventMocks.map((eventMock) => {
+    return renderEvent(eventMock);
+  });
+
+  const makeTripDestination = (input) =>`<ul class="trip-days">
+      <li class="trip-days__item day">
         <div class="day__info">
           <span class="day__counter">1</span>
-          <time class="day__date" datetime="${new Date(data.startDate).getYear() + 1900}-${new Date(data.startDate).getMonth() + 1}-${new Date(data.startDate).getDate()}">${months[new Date(data.startDate).getMonth()]} ${new Date(data.startDate).getDate()}</time>
+          <time class="day__date" datetime="${new Date(input.startDate).getYear() + 1900}-${new Date(input.startDate).getMonth() + 1}-${new Date(input.startDate).getDate()}">${months[new Date(input.startDate).getMonth()]} ${new Date(input.startDate).getDate()}</time>
         </div>
         <ul class="trip-events__list">
-          ${tripEvents(data)}
+          ${concatinatedEvents.join(``)}
+
         </ul>
       </li>
    </ul>`.trim();
--- src/main.js
+++ src/main.js
@@ -1,66 +1,17 @@
+import {data} from './components/data.js';
 import {tripInfo} from './components/trip-info.js';
 import {tripControls} from './components/trip-controls.js';
 import {tripFilters} from './components/trip-filters.js';
 import {tripEventSort} from './components/trip-event-sort.js';
 import {tripEventWrapper} from './components/trip-event-wrapper.js';
 
-const getTripDestination = {
-  transportTypes: [[`Flight`, `Train`, `Bus`],
-    [`Bus`, `Ship`, `Transport`],
-    [`Taxi`, `Train`, `Drive`]
-  ][Math.floor(Math.random() * 3)],
-  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`,
-  originalDestination: [`Amsterdam`,
-    `Dusseldorf`,
-    `Rotterdam`
-  ][Math.floor(Math.random() * 3)],
-  finallDestination: [`Munich`,
-    `Berlin`,
-    `Nur-Sultan`
-  ][Math.floor(Math.random() * 3)],
-  startDate: Date.now() + 1 + Math.floor(Math.random() * ((9 - 7) + 7)) * 24 * 60 * 60 * 1000,
-  endDate: Date.now() + 2 + Math.floor(Math.random() * ((14 - 10) + 10)) * 24 * 60 * 60 * 1000,
-  additionalOptions: [`Add meal`, `Chose seats`],
-  optionCost: [2, 9],
-
-  intermediateCities: {
-    Amsterdam: [`Einhoven`, `Antwerp`, `Duisburg`],
-    Dusseldorf: [`Cologne`, `Koblenz`, `Frankfurt am Main`],
-    Rotterdam: [`Breda`, `Bruges`, `Bonn`]
-  },
-
-  intermediatePrice: {
-    Amsterdam: [20, 16, 35],
-    Dusseldorf: [10, 25, 75],
-    Rotterdam: [5, 55, 25]
-  },
-
-  intermediateStartTime: {
-    Amsterdam: [`10:00`, `16:20`, `19:15`],
-    Dusseldorf: [`8:25`, `11:35`, `14:30`],
-    Rotterdam: [`6:40`, `8:55`, `13:25`]
-  },
-
-  intermediateEndTime: {
-    Amsterdam: [`11:10`, `18:10`, `21:45`],
-    Dusseldorf: [`8:55`, `13:05`, `17:50`],
-    Rotterdam: [`7:05`, `10:30`, `18:15`]
-  },
-
-  transportationDuration: {
-    Amsterdam: [`1H 10M`, `1H 50M`, `2H 30M`],
-    Dusseldorf: [`30M`, `1H 30M`, `3H 20M`],
-    Rotterdam: [`25M`, `1H 35M`, `4H 50M`]
-  },
-
-};
-
+const object = data();
 
 const render = (container, template, place, count) => {
   if (count !== undefined) {
     container.insertAdjacentHTML(place, new Array(count)
     .fill(``)
-    .map(getTripDestination)
+    .map(data)
     .map(template)
     .join(``));
   } else {
@@ -74,8 +25,8 @@ const tripMain = document.querySelector(`.trip-main__trip-info`);
 const siteTripControlElement = document.querySelector(`.trip-main__trip-controls`);
 const tripEvent = document.querySelector(`.trip-events`);
 
-render(tripMain, tripInfo(getTripDestination), `afterbegin`);
+render(tripMain, tripInfo(object), `afterbegin`);
 render(siteTripControlElement, tripControls(), `afterbegin`);
 render(siteTripControlElement, tripFilters(), `beforeend`);
 render(tripEvent, tripEventSort(), `afterbegin`);
-render(tripEvent, tripEventWrapper(getTripDestination), `beforeend`);
+render(tripEvent, tripEventWrapper(object), `beforeend`);
